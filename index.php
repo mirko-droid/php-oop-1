84c85ba2f454a99c738d6f332d5267d3bcb4d4a0
Mirko <mirko.saponaro91@gmail.com>
2021-02-15 17:03:35 +0100

    Stampate a schermo contenuti delle entità
    Stampate a schermo contenuti delle entità con funzione get in echo

--- index.php
+++ index.php
@@ -42,6 +42,58 @@ $film4 = new Film('Wonder Woman', 'Fantasy');
 $film5 = new Film('Lockdown Italia', 'Commedia');
 
 var_dump($film2, $film3, $film4, $film5);
-
+// inerisco in un array così da poter usare un foreach in php e stamparli a schermo con echo
+$films = [$film1, $film2, $film3, $film4, $film5];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google Faq</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+</head>
+<style>
+    *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    }
+    body{
+        max-width: 100vw;
+    }
+    .container{
+        width: 1200px;
+        margin: 0 auto;
+    }
+    .film_card{
+        height: 100px;
+        background: grey;
+        margin: 0 10px;
+    }
+</style>
+<body>
+    <div class="container"> 
+    <?php foreach ($films as $film) { ;?>
+
+        <div class="film_card">
+            <p>
+                <?php echo $film->getName() ; ?>
+            </p>
+            <p>
+                <?php echo $film->getGenere() ; ?>
+            </p>
+        </div>
+
+    <?php } ; ?>
+
+    </div>
+
+    
+   
+
+    
+</body>
+</html>
