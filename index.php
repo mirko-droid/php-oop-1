e782785dea61503334cba6dbc8acc3dd6aaa9588
Mirko <mirko.saponaro91@gmail.com>
2021-02-15 14:57:28 +0100

    inizio repo

---
+++ index.php
@@ -0,0 +1,3 @@
+<!-- Create una classe per descrivere un entitá, assegnatele degli attributi, utilizzate il constructor. Create alcune istanze della classe. Stampate a schermo gli attributi di ogni istanza.
+Per chi non ha fantasia ecco qualche entitá che potete usare:
+Animale, Persona, Casa, Computer, Hotel, Film, Videogioco, Sport, Bicicletta, Veicolo, Nazione etc. -->
